74ce092fe5978913dc26fd4265394f403a40d23a
dev-miguel-resources <ingenieromiguelch@gmail.com>
2020-05-07 21:32:04 -0400

    consumo apis, hooks y funcionalts components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {Fragment} from 'react';
+import Header from './components/Header';
+import SearchFoodDishes from './components/SearchFoodDishes';
+import Footer from './components/Footer';
+
+const App = () => (
+  <Fragment>
+    <Header />
+    <SearchFoodDishes />
+    <Footer />
+  </Fragment>
+);
+
+App.displayName = 'App';
+
+export default App;
